0126da92b04ea734ee5f0f1a94aeff954a3600f5
András Rutkai <riskawarrior@live.com>
2018-01-16 23:02:08 +0100

    Updated backend packages

--- db/db.js
+++ db/db.js
@@ -1,22 +1,23 @@
 const config = require('config');
 const MongoClient = require('mongodb').MongoClient;
 
-let dbconn;
+let client, db;
 
 exports.init = init;
 function init() {
     return MongoClient.connect(config.get('db.connectionStr'))
-        .then(db => {
-            dbconn = db;
+        .then(mongoclient => {
+            client = mongoclient;
+            db = client.db(config.get('db.collection'));
         });
 }
 
 exports.getConnection = getConnection;
 function getConnection() {
-    return dbconn;
+    return db;
 }
 
 exports.getCollection = getCollection;
 function getCollection(name) {
-    return dbconn.collection(name);
+    return db.collection(name);
 }
