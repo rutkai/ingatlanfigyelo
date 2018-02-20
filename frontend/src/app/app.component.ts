46a2b5ce4b2dcab25a04634602f889230e9ceeb9
András Rutkai <riskawarrior@live.com>
2018-02-20 23:18:48 +0100

    Added swipe gesture

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -11,8 +11,12 @@ import {MatSidenav} from "@angular/material";
 export class AppComponent implements AfterViewInit {
   @ViewChild('pageContent') pageContent: ElementRef;
   @ViewChild('sidenavMenu') sidenavMenu: MatSidenav;
+  @ViewChild('sidenavFilter') sidenavFilter: MatSidenav;
 
   public showUsermenu = false;
+
+  private swipeCoord?: [number, number];
+  private swipeTime?: number;
 
   constructor(private headUpdaterService: HeadUpdaterService,
               userStore: UserStore,
@@ -35,5 +39,31 @@ export class AppComponent implements AfterViewInit {
   public hideMenus() {
     this.sidenavMenu.close();
     this.showUsermenu = false;
+  }
+
+  public swipe(e: TouchEvent, when: string, target?: string): void {
+    const coord: [number, number] = [e.changedTouches[0].pageX, e.changedTouches[0].pageY];
+    const time = new Date().getTime();
+
+    if (when === 'start') {
+      this.swipeCoord = coord;
+      this.swipeTime = time;
+    } else if (when === 'end') {
+      const direction = [coord[0] - this.swipeCoord[0], coord[1] - this.swipeCoord[1]];
+      const duration = time - this.swipeTime;
+
+      if (duration < 1000 //Rapid
+        && Math.abs(direction[0]) > 30 //Long enough
+        && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) { //Horizontal enough
+        const swipe = direction[0] < 0 ? 'next' : 'previous';
+
+        if (target === 'menu' && swipe === 'next') {
+          this.sidenavMenu.close();
+        }
+        if (target === 'filter' && swipe === 'previous') {
+          this.sidenavFilter.close();
+        }
+      }
+    }
   }
 }
